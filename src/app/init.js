de4ed32fb411efa8cbf9181e9fb69dc02d2c3ab3
nick-cochran <c.nick@wustl.edu>
2024-01-05 21:23:35 -0600

    important change

--- src/app/init.js
+++ src/app/init.js
@@ -2,6 +2,7 @@
 import { Montserrat, Work_Sans, Inter } from 'next/font/google'
 import Image from 'next/image'
 import Link from 'next/link'
+import secrets from './secrets.js'
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
@@ -12,16 +13,7 @@ import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDBftUnYfwxsOgD1zZ-s2-5OdfT7SjnK6g",
-  authDomain: "nick-cochran-portfolio.firebaseapp.com",
-  databaseURL: "https://nick-cochran-portfolio-default-rtdb.firebaseio.com",
-  projectId: "nick-cochran-portfolio",
-  storageBucket: "nick-cochran-portfolio.appspot.com",
-  messagingSenderId: "341628610107",
-  appId: "1:341628610107:web:518b9aa9b4bf94a5221e1e",
-  measurementId: "G-M925RDGPVF"
-};
+const firebaseConfig = secrets.firebaseConfig;
 
 let firebaseApp;
 let analytics;
